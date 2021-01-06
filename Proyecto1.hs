06a09b3c59dd1e78564f089a4b2f4de191d46adc
Alanoterohs <alanoterohs@gmail.com>
2021-01-06 10:16:27 -0300

    ejercicios del 2 al 6

--- Proyecto1.hs
+++ Proyecto1.hs
@@ -57,3 +57,81 @@ promedio :: [Int] -> Int
 promedio [] = 0
 promedio (x:xs) = sumatoria (x:xs) `div` length (x:xs)
 
+
+--Ejercicio 3
+
+--pertenece
+pertenece :: Int -> [Int] -> Bool
+pertenece _ []       = False
+pertenece x (y:ys)   = x==y || elem x ys
+
+
+--Ejercicio 4
+
+--paratodo'
+
+paratodo' :: [a] -> (a->Bool) -> Bool 
+paratodo' [] t = True
+paratodo' (x:xs) t = t x && paratodo' xs t
+
+--existe
+
+existe' :: [a] -> (a -> Bool) -> Bool
+existe' [] t = False
+existe' (x:xs) t = t x || existe' xs t
+
+--sumatoria
+
+sumatoria' :: [a] -> (a -> Int) -> Int 
+sumatoria' [] t = 0
+sumatoria' (x:xs) t = (t x) + sumatoria' xs t
+
+--productoria
+
+productoria' :: [a] -> (a -> Int) -> Int 
+productoria' [] t = 1
+productoria' (x:xs) t = (t x) * productoria' xs t
+
+
+--Ejercicio 5
+
+paratodo'' :: [Bool] -> Bool
+paratodo'' xs = paratodo' xs id
+
+--Ejercicio 6
+
+--todosPares
+
+todosPares :: [Int] -> Bool 
+todosPares xs = paratodo' xs even
+
+--hayMultiplo
+
+
+--funcion auxiliar
+esMultiplo :: Int -> Int -> Bool 
+esMultiplo 0 0 = False
+esMultiplo x y | (x `mod` y == 0) = True 
+               | otherwise = False 
+
+hayMultiplo :: Int -> [Int] -> Bool
+hayMultiplo x xs = existe' xs (esMultiplo x)
+
+--SumaCuadrados
+ 
+sumaCuadrados :: Int -> Int
+sumaCuadrados x = sumatoria' [x*x | x <- [1..x]] id
+
+--factorial'
+
+factorial2 :: Int -> Int
+factorial2 x = productoria' [x | x <- [1..x]] id
+
+--multiplicaPares
+--funcion aux
+esPar :: Int -> Int
+esPar (x) | even x = x
+          | otherwise = 1
+                 
+multiplicaPares :: [Int] -> Int
+multiplicaPares xs =  productoria' xs esPar
